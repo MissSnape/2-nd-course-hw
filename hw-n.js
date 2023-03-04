03fd7b57422a9163f747c57b013da4b329604d93
MissSnape <gerasimovaanna258@gmail.com>
2023-03-05 02:32:59 +0400

    вуф

--- hw-n.js
+++ hw-n.js
@@ -1,95 +1,75 @@
 //Задание 1
-let a=prompt("Введите число");
-let b=prompt("Введите второе число");
-function mostNumber(a,b){
-    
-    if (a>b) {
-        console.log(a);
-    } else {
-        console.log(b);
-    }
-}
-mostNumber(a,b);
+const arr=[1,5,4,10,0,3];
+for (let i=0; i<arr.length; i++) {
+   if (arr[i]==0) break;
+    console.log(arr[i]);
+};
 //задание 2
-"use strict";
-let number = prompt("Введите число");
-
-let printMessage;
-
-if (number%2===0) {
-
-    printMessage = function () {
-        console.log("Число четное");
-    }
-
-} else {
-
-    printMessage = function () {
-        console.log("Число нечетное");
-    }
-
-}
-
-printMessage();
-
+const massif=[1, 5, 4, 10, 0, 3];
+console.log("Индекс 4 равен "+massif.findIndex(i => i == "4"));
 //задание 3
-let p=4;
-function squareReturn(p) { 
-    return p**2 
-}
-let result1 = squareReturn(4); 
-alert(result1);
-
+let massiv=[1, 3, 5, 10, 20];
+massiv=massiv.join(' ');
+console.log(massiv);
 //задание 4
-"use strict";
-let age = prompt("Введите возраст",13);
-
-let printReport;
-
-
-if (age>12) {
-
-    printReport = function () {
-        console.log("Добро пожаловать");
+let nesting = [];
+for (let i = 1; i<=3; i++)
+{
+    nesting[i] = [1,1,1];
+    for (let j = 1; j<i; j++) {
+        nesting[i][j] = 1;
     }
-
-} else if (age<0){
-
-    printReport = function () {
-        console.log("Вы ввели неправильное значение");
-    }
-    
-
-} else {
-    printReport= function(){
-        console.log("Привет,друг!");
-    }
-
 }
-
-printReport();
+console.log(nesting);
 //задание 5
-let one=prompt("Введи число");
-let two=prompt("Введи число");
-function mult (one, two) { 
-    if(!isNaN(one) && !isNaN(two)) { 
-       return alert(one * two); 
-}    else { 
-        alert ("Одно или оба значения не являются числом"); 
-}
-}
-mult (one, two);
+let a=[1,1,1];
+a.push(2,2,2);
+console.log(a);
 //задание 6
-let num=prompt("Введите число");
-function cube(num) {
-    if (!isNaN(num)) {
-    return alert("n в кубе равняется "+Math.pow(num,3));
-    } else {
-    console.log('Переданный параметр не является числом');
-    }
-}
-cube(num);
+let c=[9,8,7,'a',6,5];
+c.sort();
+c.pop();
+console.log(c);
 //задание 7
+const b=[9,8,7,6,5];
+let d=prompt('Введите число');
+d=Number(d);
+const test=b.filter(el=>el===d);
+console.log(test);
+//задание 8
+let letters='abcdf';
+letters=letters.split(' ');
+letters=letters.reverse();
+letters=letters.join(' ');
+console.log(letters);
+//задание 9
+let numeral=[[1,2,3],[4,5,6]];
+numeral=numeral.join(',');
+console.log(numeral);
+//задание 10
+let arbitrary=[1,5,6,8,2];
+for(let i=1; i<11; i=i+1){
+
+    const f=arbitrary.map(el=>el+1);
+    console.log(f);
+}
+//задание 11
+let w=[];
+for(let i=0; i<=10;i++){
+    w.push(Math.floor(Math.random()*i));
+}
+const even=w.filter(el=>el%2===0);
+console.log("Исходный массив "+w);
+console.log("Массив с четными числами "+even);
+//задание 12
+let m=[];
+for(let i=0; i<=6;i++){
+    m.push(Math.floor(Math.random()*i));
+
+}
+const result = m.reduce((a, b) => a + b, 0) / m.length;
+console.log(result)
+//задание hw-5
 function startGameSeasons(){
     let month=prompt("Введите месяц");
     month=Number(month);
