e3ec61b1640f0b9b415012b0c14e410faf0a9cdc
MissSnape <gerasimovaanna258@gmail.com>
2023-03-08 20:06:35 +0400

    added

--- hw-n.js
+++ hw-n.js
@@ -1,75 +1,80 @@
 //Задание 1
-const arr=[1,5,4,10,0,3];
-for (let i=0; i<arr.length; i++) {
-   if (arr[i]==0) break;
-    console.log(arr[i]);
-};
+function uppercase(){
+    let a=prompt("Введите слово");
+    console.log(a.toUpperCase());
+}
+uppercase();
 //задание 2
-const massif=[1, 5, 4, 10, 0, 3];
-console.log("Индекс 4 равен "+massif.findIndex(i => i == "4"));
+
+//console.log(regexp);
+
 //задание 3
-let massiv=[1, 3, 5, 10, 20];
-massiv=massiv.join(' ');
-console.log(massiv);
+let a=32.58884;
+console.log(Math.floor(a));
+console.log(Math.ceil(a));
+console.log(Math.round(a));
 //задание 4
-let nesting = [];
-for (let i = 1; i<=3; i++)
-{
-    nesting[i] = [1,1,1];
-    for (let j = 1; j<i; j++) {
-        nesting[i][j] = 1;
-    }
-}
-console.log(nesting);
+let arr=[ 52, 53, 49, 77, 21, 32];
+const minimum = Math.min(...arr);
+console.log(minimum);
+const maxsimum=Math.max(...arr);
+console.log(maxsimum);
 //задание 5
-let a=[1,1,1];
-a.push(2,2,2);
-console.log(a);
+function EnterRandom(){
+    let c=Math.round(Math.random()*10);
+    console.log(c);
+}
+EnterRandom();
 //задание 6
-let c=[9,8,7,'a',6,5];
-c.sort();
-c.pop();
-console.log(c);
+function getRandomArrNumbers(){
+let p=prompt("Введите число");
+let numbers=[];
+for (let i=0; i<p; i++) {
+numbers.push(Math.round(Math.random()*(p)));
+console.log(numbers);
+}
+}
+getRandomArrNumbers();
 //задание 7
-const b=[9,8,7,6,5];
-let d=prompt('Введите число');
-d=Number(d);
-const test=b.filter(el=>el===d);
-console.log(test);
+function MinMax(min,max){
+   console.log( Math.random()*(max-min));
+}
+MinMax(11,100);
 //задание 8
-let letters='abcdf';
-letters=letters.split(' ');
-letters=letters.reverse();
-letters=letters.join(' ');
-console.log(letters);
+let currentDate = new Date();
+console.log(currentDate);
 //задание 9
-let numeral=[[1,2,3],[4,5,6]];
-numeral=numeral.join(',');
-console.log(numeral);
+let newDates= new Date();
+newDates.setDate(newDates.getDate() + 75);
+console.log(newDates);
 //задание 10
-let arbitrary=[1,5,6,8,2];
-for(let i=1; i<11; i=i+1){
+const days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+const months = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня",
+"Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
+let myDate = new Date(); 
+let fullDate = "Сегодня: " + myDate.getDate() + " " + months[myDate.getMonth()] + " " + myDate.getFullYear() +", " + days[myDate.getDay()];
+console.log(fullDate); 
+let presentTime= new Date;
+console.log(presentTime.toTimeString());
 
-    const f=arbitrary.map(el=>el+1);
-    console.log(f);
-}
 //задание 11
-let w=[];
-for(let i=0; i<=10;i++){
-    w.push(Math.floor(Math.random()*i));
+function fruit(){
+let fruits=['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин']
+fruits.sort(()=>Math.random()-0.5);
+alert(fruits);
+let a=prompt("Чему равнялся первый элемент массива?");
+let b=prompt("Чему равнялся последний элемент массива?");
+if (fruits[0] === a && fruits[fruits.length - 1] === b) {
+	console.log("Поздравляю, вы выиграли")
+} else if (fruits[0] !== a || fruits[fruits.length - 1] !== b) {
+	console.log("Вы были близки")
+} else {
+	console.log("Вы проиграли")
 }
-const even=w.filter(el=>el%2===0);
-console.log("Исходный массив "+w);
-console.log("Массив с четными числами "+even);
-//задание 12
-let m=[];
-for(let i=0; i<=6;i++){
-    m.push(Math.floor(Math.random()*i));
+
 
 }
 
-const result = m.reduce((a, b) => a + b, 0) / m.length;
-console.log(result)
 //задание hw-5
 function startGameSeasons(){
     let month=prompt("Введите месяц");
